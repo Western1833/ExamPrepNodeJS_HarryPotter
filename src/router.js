edae9c52fa36c48c26b757187b5911e090ed1c83
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-10-15 16:35:28 +0300

    Added userController and some fixes

--- src/router.js
+++ src/router.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const homeController = require('./controllers/homeController.js');
+const userController = require('./controllers/userController.js');
 
 //Home controller routes
 router.use(homeController);
+
+//User controller routes
+router.use('/users', userController);
 
 module.exports = router;
