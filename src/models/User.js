45e97911dac04bee69871bb374146b5314f843c8
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-10-15 20:34:54 +0300

    Validate if email already exists

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
+    email: {type: String, required: true, 
+        unique: {value: true, message: 'Email already exists!'}},
     password: {type: String, required: true},
 });
 
