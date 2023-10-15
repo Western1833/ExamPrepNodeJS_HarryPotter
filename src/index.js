401124e7ea575028aaf656e9cfdf3f7a1e3641b6
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-10-15 10:24:14 +0300

    added 'path' to point out static files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const configFile = require('./constants.js');
+const path = require('path');
 
 const app = express();
 
+//Express configurations/middlewares
+app.use(express.static(path.resolve(__dirname, './public')))
 app.use(express.urlencoded({extended: false})); 
 
+//Paths
 app.get('/', (req, res) => {
     res.send('<h1>Hello</h1>');
 });
