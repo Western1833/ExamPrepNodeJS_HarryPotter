a149f6436fbccec5a942a66e2283e0b5f21b6832
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-10-15 14:00:05 +0300

    Install mongoose and initalize

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 const CONFIG = {
     PORT: 5030,
+    DB_URL: 'mongodb://localhost:27017/wizard-creatures'
 };
 
 module.exports = CONFIG;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const configFile = require('./constants.js');
 const handlebars = require('express-handlebars');
+const mongoose = require('mongoose');
 const path = require('path');
 const router = require('./router.js');
 
@@ -15,6 +16,18 @@ app.engine('hbs', handlebars.engine({extname: 'hbs'}));
 app.set('view engine', "hbs");
 app.set('views', 'src/views');
 
+//DB connection
+async function dbConnect(){
+    await mongoose.connect(configFile.DB_URL);
+}
+
+dbConnect()
+.then(() => {
+    console.log('Successfully connected to database.')
+})
+.catch(err => console.log(`Error while connectig to database: ${err}`));
+
+//Routing
 app.use(router);
 
 app.listen(configFile.PORT, () => console.log(`Server is listening on Port: ${configFile.PORT}...`));
